0980876aab5f43bd63f9a54c54481fb26796a39c
galshaya <42077171+galshaya@users.noreply.github.com>
2025-02-23 12:19:58 -0500

    asteroids commit 2

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Create our player at the center of the screen
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    
+    # Create groups and set up containers
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroid, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    asteroidfeild = AsteroidField()
+
+
     while True:
+        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        
+        # Clear screen with black backgroundß
+        screen.fill((0, 0, 0))
+        
+        # Update all objects
+        updatable.update(dt)
+        
+        # Draw all objects
+        for drawable_obj in drawable:
+            drawable_obj.draw(screen)
+        
+        # Update display
         pygame.display.flip()
+        
+        # Control frame rate and get dt
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
